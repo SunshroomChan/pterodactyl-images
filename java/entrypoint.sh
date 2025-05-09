f2874cd27bc82847fa0b4c2fb1a879c219978c58
HoangNotGay <89954408+SunshroomChan@users.noreply.github.com>
2025-05-09 09:58:05 +0700

    Updated Java entrypoint.sh

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/container || exit 1
 
-# Configure colors
+# Configure colors (sh không có \e, nên dùng printf cho ANSI)
 CYAN='\033[0;36m'
 RESET_COLOR='\033[0m'
 
@@ -12,11 +12,11 @@ java -version
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Replace Startup Variables
+# Replace Startup Variables (sh không cần echo -e, dùng printf)
 # shellcheck disable=SC2086
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
+MODIFIED_STARTUP=$(printf '%s\n' "$STARTUP" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+printf '%sSTARTUP /home/container: %s %s\n' "$CYAN" "$MODIFIED_STARTUP" "$RESET_COLOR"
 
 # Run the Server
 # shellcheck disable=SC2086
-eval ${MODIFIED_STARTUP}
+eval "$MODIFIED_STARTUP"
