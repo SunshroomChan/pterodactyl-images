aa6cb538b60c612a761b701351a2c9d1240abd0c
HoangNotGay <89954408+SunshroomChan@users.noreply.github.com>
2025-05-09 10:12:17 +0700

    Fully entrypoint.sh support

--- java-openj9/entrypoint.sh
+++ java-openj9/entrypoint.sh
@@ -15,7 +15,7 @@ export INTERNAL_IP
 # Replace Startup Variables
 # shellcheck disable=SC2086
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
+printf '%sSTARTUP /home/container: %s %s\n' "$CYAN" "$MODIFIED_STARTUP" "$RESET_COLOR"
 
 # Run the Server
 # shellcheck disable=SC2086
